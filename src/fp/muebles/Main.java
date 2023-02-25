8f328f1ffb93226164c3c83f9d22afaf0e9df242
Miguel Garcia Vizcaino <miguelgvizcaino@gmail.com>
2023-02-25 13:10:31 +0100

    Objeto Mueble

--- src/fp/muebles/Main.java
+++ src/fp/muebles/Main.java
@@ -1,7 +1,15 @@
 package fp.muebles;
 
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args){
+        Muebles sofa = new Muebles();
+        String sofa_ship = getShipmode();
+        System.out.println(sofa_ship);
+    }
 
+    public String getShipmode(){
+        return this.shipmode;
     }
 }
--- src/fp/muebles/Muebles.java
+++ src/fp/muebles/Muebles.java
@@ -1,7 +1,13 @@
 package fp.muebles;
 
+import java.time.LocalDate;
+import java.util.spi.LocaleNameProvider;
+
 public class Muebles {
-    String shipmode;
+    public Muebles(){
+        System.out.println("Nuevo Objeto Mueble Creado");
+    }
+    private String shipmode;
     int ventas;
     int calidad;
     int descuento;
@@ -10,5 +16,5 @@ public class Muebles {
     String region;
     String estado;
     String subcategoria;
-
+    LocalDate dia;
 }
